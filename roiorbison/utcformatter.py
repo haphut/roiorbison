1e55d345f5518c27c482064429ce0e8445aa342c
haphut <haphut@gmail.com>
2016-08-31 22:57:48 +0300

    Unwrap logging setup function

---
+++ roiorbison/utcformatter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""UTC timestamps for logging."""
+
+import logging
+import time
+
+
+class UTCFormatter(logging.Formatter):
+    """Use UTC time instead of local time."""
+    converter = time.gmtime
