9a15907aa6a8ce083eeceeb7ae7e30294bd4e1ae
haphut <haphut@gmail.com>
2016-09-02 11:24:17 +0300

    Refactor util.py

--- roiorbison/util.py
+++ roiorbison/util.py
@@ -3,7 +3,6 @@
 
 import asyncio
 import concurrent.futures
-import functools
 
 import isodate
 
@@ -20,32 +19,28 @@ class AsyncHelper:
         self.loop = loop
         self.executor = executor
 
-    async def wait_until_first_done(self, futures, logger):
-        """Wait until the first future completes or raises an exception.
-
-        We do not expect any future to raise an exception, so log a warning if
-        that happens.
-        """
-        wait = functools.partial(asyncio.wait, loop=self.loop, timeout=None)
-        completed = wait(
-            futures, return_when=concurrent.futures.FIRST_COMPLETED)
-        raised = wait(futures, return_when=concurrent.futures.FIRST_EXCEPTION)
-        done, dummy_pending = await wait(
-            [completed, raised],
-            return_when=concurrent.futures.FIRST_COMPLETED)
-        if raised in done:
-            logger.warning(
-                'An awaitable raised an exception but was not supposed to: ' +
-                str(raised))
+    async def ensure_future(self, coro_or_future, *args):
+        """Create a task."""
+        return await asyncio.ensure_future(
+            coro_or_future, *args, loop=self.loop)
 
     async def run_in_executor(self, func, *args):
         """Use asyncio.run_in_executor() easily."""
         return await self.loop.run_in_executor(self.executor, func, *args)
 
-    async def wait_for_event(self, event, *args, **kwargs):
-        """Use threading.Event.wait() with asyncio."""
-        return await self.run_in_executor(event.wait, *args, **kwargs)
+    async def wait_for_first(self, futures, *args):
+        """Wait until the first future finishes."""
+        return await asyncio.wait(
+            futures,
+            *args,
+            loop=self.loop,
+            timeout=None,
+            return_when=concurrent.futures.FIRST_COMPLETED)
 
     async def wait_forever(self, future):
         """Wait for a future until it is done."""
         return await asyncio.wait_for(future, timeout=None, loop=self.loop)
+
+    async def wait_for_event(self, event, *args):
+        """Use threading.Event.wait() with asyncio."""
+        return await self.run_in_executor(event.wait, *args)
